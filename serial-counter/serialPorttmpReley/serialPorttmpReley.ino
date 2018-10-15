6dbda1bcdce6f578c13ed870c09face29601326c
IOT <kpinedac2@miumg.edu.gt>
2018-10-14 20:36:50 -0600

    Primer Commit

---
+++ serial-counter/serialPorttmpReley/serialPorttmpReley.ino
@@ -0,0 +1,32 @@
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
+#define SENSOR 0
+#define RELEY_1 12
+#define RELEY_2 11
+
+void setup() {
+Serial.begin(9600);
+pinMode(RELEY_1, OUTPUT);
+pinMode(RELEY_2, OUTPUT);
+}
+
+int signalVoltage, celsiusTemp;
+
+void loop() {
+
+celsiusTemp= analogRead(SENSOR);
+celsiusTemp=(5*signalVoltage*100)/1024;
+
+Serial.print(celsiusTemp);
+
+if(celsiusTemp>26)
+{
+  digitalWrite(RELEY_1, 0);
+  }
+  else
+  {
+      digitalWrite(RELEY_1, 1);
+    }
+}
